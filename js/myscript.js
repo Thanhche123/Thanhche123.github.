7d40c5b1e33ff76280f89dd88fb3803d1648b0ee
Thanhche123 <122333021+Thanhche123@users.noreply.github.com>
2023-01-10 10:36:36 +0700

    upload

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".navbar a, footer a[href='#myPage']").on("click", function(event){
+        if(this.hash !== "") {
+            event.prevenDefault();
+            var hash = this.hash;
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                900,
+                function(){
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
+});
